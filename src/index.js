895263bea030fde6cd8982471cf10086d0db5de2
Olga Hajduk <hajduk.o.a@gmail.com>
2021-02-04 14:35:12 +0300

    feat: add solve for task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+
+    const empty = !Array.isArray(matrix) || (matrix.length === 0)
+
+    if (empty) {
+
+        return []
+
+    } else {
+
+        matrix.forEach((value, i) => {
+            if (i % 2 !== 0) {
+                matrix[i].reverse(value)
+            }
+        });
+
+        const matrixSorted = matrix.reduce((accuracy, value) => accuracy.concat(value), [])
+
+        return matrixSorted
+    }
 }
